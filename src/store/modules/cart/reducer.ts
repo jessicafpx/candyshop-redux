3ebc27b10d474f9a979dca445a7f927a4614756f
Jessica Peixoto <jessica.fpeixoto@gmail.com>
2021-01-19 16:05:53 -0300

    feat: update state with immer

--- src/store/modules/cart/reducer.ts
+++ src/store/modules/cart/reducer.ts
@@ -1,12 +1,29 @@
 import { Reducer } from 'redux';
+import produce from 'immer';
 import { ICartState } from "./types";
 
 const INITIAL_STATE: ICartState = {
-    items: []
+  items: []
 };
 
-const cart: Reducer<ICartState> = (state, action) => {
-    return INITIAL_STATE;
+const cart: Reducer<ICartState> = (state = INITIAL_STATE, action) => {
+  return produce(state, draft => {
+    switch (action.type) {
+      case 'ADD_PRODUCT_TO_CART': {
+        const { product } = action.payload;
+
+        draft.items.push({
+          product,
+          quantity: 1,
+        });
+
+        break;
+      }
+      default: {
+        return state;
+      }
+    }
+  });
 }
 
 export default cart;
