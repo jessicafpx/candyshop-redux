4f1e22534a4c3fa307e5ffd33ede54b8438387b2
Jessica Peixoto <jessica.fpeixoto@gmail.com>
2021-01-19 16:13:49 -0300

    feat: increment quantity of items in the cart

--- src/store/modules/cart/reducer.ts
+++ src/store/modules/cart/reducer.ts
@@ -12,10 +12,18 @@ const cart: Reducer<ICartState> = (state = INITIAL_STATE, action) => {
       case 'ADD_PRODUCT_TO_CART': {
         const { product } = action.payload;
 
-        draft.items.push({
-          product,
-          quantity: 1,
-        });
+        const productInCartIndex = draft.items.findIndex(item =>
+          item.product.id === product.id,
+        );
+
+        if (productInCartIndex >= 0) {
+          draft.items[productInCartIndex].quantity++;
+        } else {
+          draft.items.push({
+            product,
+            quantity: 1,
+          });
+        }
 
         break;
       }
