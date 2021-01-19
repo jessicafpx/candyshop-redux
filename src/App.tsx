e7a176d5c35d51408a6bad09ee3343990b5a7bf6
Jessica Peixoto <jessica.fpeixoto@gmail.com>
2021-01-19 16:25:46 -0300

    feat: add Cart component

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,13 @@ import { Provider } from 'react-redux';
 
 import store from './store';
 import Catalog from './components/Catalog';
+import Cart from './components/Cart';
 
 function App() {
   return (
     <Provider store={store}>
       <Catalog />
+      <Cart />
     </Provider>
   );
 }
