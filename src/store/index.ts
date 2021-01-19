38c8c1dee6fa44c90349a260a11c39736da7bec5
Jessica Peixoto <jessica.fpeixoto@gmail.com>
2021-01-19 16:26:42 -0300

    feat: add store index

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,7 +1,17 @@
 import { createStore } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
 
 import rootReducer from './modules/rootReducer';
 
-const store = createStore(rootReducer);
+import { ICartState } from './modules/cart/types';
+
+export interface IState {
+  cart: ICartState;
+}
+
+const store = createStore(
+  rootReducer,
+  composeWithDevTools()
+);
 
 export default store;
