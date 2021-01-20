498f29d46f243210369eef0693ea1d2a79785c15
Jessica Peixoto <jessica.fpeixoto@gmail.com>
2021-01-20 20:14:34 -0300

    style: add global styles

---
+++ src/styles/global.ts
@@ -0,0 +1,29 @@
+import { createGlobalStyle } from 'styled-components';
+
+export default createGlobalStyle`
+  :root {
+    font-size: 62.5%;
+  }
+
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    outline: 0;
+  }
+
+  body {
+    background: #FFF5F5;
+    -webkit-font-smoothing: antialiased;
+
+    font-family: 'Poppins', sans-serif;
+  }
+
+  h1, h2, h3, h4, h5, h6, strong {
+    font-weight: 400;
+  }
+
+  button {
+    cursor: pointer;
+  }
+`;
