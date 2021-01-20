4c4885b7f2fd066d9a1dc7e11febfc475aae5471
Jessica Peixoto <jessica.fpeixoto@gmail.com>
2021-01-20 10:24:46 -0300

    feat: get quantity of products from the cart

--- src/store/modules/cart/sagas.ts
+++ src/store/modules/cart/sagas.ts
@@ -1,6 +1,18 @@
-import { all, takeLatest } from 'redux-saga/effects';
+import { all, takeLatest, select } from 'redux-saga/effects';
+import { addProductToCart } from './actions';
+import { IState } from '../../';
 
-function checkProductStock() {
+type checkProductStockRequest = ReturnType<typeof addProductToCart>;
+
+function* checkProductStock({ payload }: checkProductStockRequest) {
+  const { product } = payload;
+
+  const currentQuantity: number = yield select((state: IState) => {
+    return state.cart.items.find(item => item.product.id === product.id)?.quantity ?? 0;
+  });
+
+  console.log(currentQuantity);
+
   console.log('Adicionou ao carrinho');
 }
 
