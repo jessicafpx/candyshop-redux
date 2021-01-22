71d2eaad7d163862e7581a5759d9c1098a539284
Jessica Peixoto <jessica.fpeixoto@gmail.com>
2021-01-22 18:45:12 -0300

    style: remove borders and fix spaces

--- src/components/Catalog/styles.ts
+++ src/components/Catalog/styles.ts
@@ -1,25 +1,20 @@
 import styled from 'styled-components';
 
-
 export const Container = styled.div`
   width: 100vw;
-  height: 100vh;
+  height: 90vh;
   max-width: 1280px;
   margin: 0 auto;
   padding: 6rem 0;
-
-  border: 1px solid yellow;
 `;
 
 export const Header = styled.header`
   display: flex;
   align-items: center;
   justify-content: left;
-  margin-left: -36px ;
-  border: 1px solid green;
+  margin-left: -7.5rem ;
 
   svg{
-    border: 1px solid red;
     margin-right: 3.6rem;
   }
 
