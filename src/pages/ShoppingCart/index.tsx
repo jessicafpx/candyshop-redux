03be592615001353e896e9d275941df581b89aae
Jessica Peixoto <jessica.fpeixoto@gmail.com>
2021-01-24 15:15:56 -0300

    feat: update page components

--- src/pages/ShoppingCart/index.tsx
+++ src/pages/ShoppingCart/index.tsx
@@ -1,13 +1,44 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
-import Catalog from '../../components/Catalog/index';
+import { Link } from 'react-router-dom';
+
+
 import Cart from '../../components/Cart/index';
 
-// import { Container } from './styles';
+import { Container, Header, Content } from './styles';
+
+import api from '../../services/api';
+
+import { IProduct } from '../../store/modules/cart/types';
+import CatalogItem from '../../components/CatalogItem';
+
+import { FiArrowLeft } from 'react-icons/fi';
 
 const ShoppingCart: React.FC = () => {
+  const [catalog, setCatalog] = useState<IProduct[]>([]);
+
+  useEffect(() => {
+    api.get('products').then(response => {
+      setCatalog(response.data);
+    })
+  }, []);
+
   return <div>
-    <Catalog />
+    <Container>
+      <Header>
+        <Link to="/">
+          <FiArrowLeft size={36} color="#FD7C5C" />
+        </Link>
+        <h3>Shopping cart</h3>
+      </Header>
+
+      <Content>
+        {catalog.map(product => (
+          <CatalogItem key={product.id} product={product} />
+        ))}
+      </Content>
+
+    </Container>
     <Cart />;
   </div>
 }
