b8d7e4d2f7a61dab4e2bd28cd53a7ce0f0ef6d87
Jessica Peixoto <jessica.fpeixoto@gmail.com>
2021-01-24 15:18:36 -0300

    style: update cart style

--- src/components/Cart/styles.ts
+++ src/components/Cart/styles.ts
@@ -1,24 +1,21 @@
 import styled from 'styled-components';
 
 export const Container = styled.footer`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
   position: fixed;
   bottom: 0;
   width: 100vw;
-  height: 8rem;
+  height: 6rem;
+  padding: 0 11.5rem;
+
   background: linear-gradient(90deg, #F9C585 0%, #F797CB 100%);
 
   color: #fff;
-  font-size: 2.4rem;
+  font-size: 2rem;
   font-weight: 700;
-
-  section {
-    max-width: 1280px;
-    margin: 0 auto;
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    height: 100%;
-  }
 `;
 
 export const TotalItens = styled.div`
