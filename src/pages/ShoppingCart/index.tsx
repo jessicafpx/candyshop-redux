95ceabd9902210abd94c2cdaab93f4c79518df9c
Jessica Peixoto <jessica.fpeixoto@gmail.com>
2021-01-25 20:55:22 -0300

    feat: add carousel

--- src/pages/ShoppingCart/index.tsx
+++ src/pages/ShoppingCart/index.tsx
@@ -1,21 +1,56 @@
 import React, { useEffect, useState } from 'react';
-
 import { Link } from 'react-router-dom';
-
-
-import Cart from '../../components/Cart/index';
-
-import { Container, Header, Content } from './styles';
-
-import api from '../../services/api';
 
 import { IProduct } from '../../store/modules/cart/types';
 import CatalogItem from '../../components/CatalogItem';
+import Cart from '../../components/Cart/index';
+
+import api from '../../services/api';
+
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
 
 import { FiArrowLeft } from 'react-icons/fi';
 
+import { Header, Content } from './styles';
+
+
 const ShoppingCart: React.FC = () => {
   const [catalog, setCatalog] = useState<IProduct[]>([]);
+
+  let settings = {
+    arrows: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    centerPadding: "50px",
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 4,
+          slidesToScroll: 1,
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+        }
+      },
+      {
+        breakpoint: 500,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          initialSlide: 0,
+        }
+      }
+    ]
+  }
 
   useEffect(() => {
     api.get('products').then(response => {
@@ -23,24 +58,27 @@ const ShoppingCart: React.FC = () => {
     })
   }, []);
 
-  return <div>
-    <Container>
+  return (
+    <>
       <Header>
         <Link to="/">
-          <FiArrowLeft size={36} color="#FD7C5C" />
+          <FiArrowLeft size={32} color="#FD7C5C" />
         </Link>
         <h3>Shopping cart</h3>
       </Header>
 
       <Content>
-        {catalog.map(product => (
-          <CatalogItem key={product.id} product={product} />
-        ))}
+        <Slider {...settings}>
+          {catalog.map(product => (
+            <CatalogItem key={product.id} product={product} />
+          ))}
+        </Slider>
       </Content>
 
-    </Container>
-    <Cart />;
-  </div>
+      <Cart />
+    </>
+  );
+
 }
 
 export default ShoppingCart;
