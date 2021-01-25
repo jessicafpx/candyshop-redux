98d0a88df575625b02da8953d8a659be63ecba13
Jessica Peixoto <jessica.fpeixoto@gmail.com>
2021-01-25 20:55:51 -0300

    style: add carousel style

--- src/pages/ShoppingCart/styles.ts
+++ src/pages/ShoppingCart/styles.ts
@@ -1,19 +1,103 @@
 import styled from 'styled-components';
 
-export const Container = styled.div`
-  width: 100vw;
-  padding: 2rem 11.5rem 0 11.5rem;
+export const Content = styled.main`
+  margin: 0 auto;
+  overflow-x: hidden;
+  height: 81vh;
 
+  .slick-slider .slick-initialized {
+    position: relative;
+    z-index: -1;
+  }
+
+  .slick-prev:before, .slick-next:before {
+    color: #fff;
+    font-size: 5rem;
+    position: absolute;
+    top: -70%;
+  }
+
+  button.slick-prev {
+    position: absolute;
+    z-index: 1;
+    filter: drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.15));
+  }
+
+  button.slick-next {
+    position: absolute;
+    z-index: 1;
+    filter: drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.15));
+  }
+
+  .slick-prev:before {
+    z-index: 1;
+    right: -9vw;
+    position: absolute;
+  }
+
+  .slick-next:before {
+    z-index: 1;
+    position: absolute;
+    left: -9vw;
+  }
+
+  .slick-list {
+    margin: 0 auto;
+    height: 475px;
+    min-width: 30rem;
+
+    .slick-slide {
+      margin: 0 3rem;
+    }
+
+   }
+
+   @media (max-width: 1235px) {
+    .slick-list .slick-slide {
+      margin: 0 5rem;
+    }
+   }
+
+   @media (max-width: 825px) {
+    .slick-list .slick-slide {
+      margin: 0 6rem;
+    }
+   }
+
+   @media (max-width: 745px) {
+    .slick-list .slick-slide {
+      margin: 0rem 7rem;
+    }
+   }
+
+   @media (max-width: 670px) {
+    .slick-list .slick-slide {
+      margin: 0rem 8rem;
+    }
+   }
+
+   @media (max-width: 600px) {
+    .slick-list .slick-slide {
+      margin: 0rem 6rem;
+    }
+   }
+
+   @media (max-width: 560px) {
+    .slick-list .slick-slide {
+      margin: 0rem 8rem;
+    }
+   }
 `;
 
 export const Header = styled.header`
   display: flex;
+  padding: 2rem 11.5rem 2rem 10.5rem;
   align-items: center;
   justify-content: left;
-  margin-left: -7.5rem ;
+  margin-left: -4rem ;
 
   svg{
-    margin-right: 3.6rem;
+    margin-right: 2rem;
   }
 
   h3 {
@@ -23,11 +107,3 @@ export const Header = styled.header`
   }
 `;
 
-export const Content = styled.main`
-  display: flex;
-  grid-template-columns: repeat(3, 1fr);
-  gap: 12rem;
-
-  overflow-x: hidden;
-  margin-top: 2rem;
-`;
